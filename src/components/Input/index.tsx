0a3205f666dbaf7e6fc7fd9d0aba80d421e052ee
caiosw <caiosw@gmail.com>
2021-05-04 02:52:11 -0300

    Input done

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,19 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  icon: string;
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+interface inputRefProps extends HTMLInputElement {
+  value: string;
+}
+
+const Input = ({ name, /*icon: Icon,*/ ...rest }: InputProps) => {
+  const inputRef = useRef<inputRefProps>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -37,7 +48,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
+      {/* {Icon && <Icon size={20} />} */}
 
       <input
         onFocus={handleInputFocus}
